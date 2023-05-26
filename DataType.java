8cf3a34ce72a997cc3cde3f8d8c366bd5fd16c86
Koketsu Ryuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-05-26 23:10:22 +0900

    初コミット

---
+++ DataType.java
@@ -0,0 +1,16 @@
+class DataType {
+  public static void main(String[] args) {
+    int month = 12;
+    int day = 29;
+    float weight = 58.0f;
+    double height = 171.5;
+    char bloodType = 'B';
+
+    String name = "纐纈";
+
+    System.out.println("こんにちは！" + name + "です");
+    System.out.println("身長は" + height + "cm、体重は" + weight + "kg、");
+    System.out.println("誕生日は" + month + "月" + day + "日、");
+    System.out.println("血液型は" + bloodType + "型です。");
+  }
+}
