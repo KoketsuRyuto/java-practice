1b27e5b0a209acc0844f8a4e1bb3cc8420383bac
Koketsu Ryuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-06-11 22:47:38 +0900

    オーバーロード

--- StuSample2.java
+++ StuSample2.java
@@ -1,13 +1,8 @@
-class StuSample2 {
+public class StuSample2 {
   public static void main(String[] args) {
     Student2 stu1 = new Student2();
-    Student2 stu2 = new Student2();
-
-    stu1.setData("ジョン");
-    stu1.setScore(90, 80);
+    stu1.setName("ジョン");
+    stu1.setStuNo(1);
     stu1.display();
-    
-    stu2.setData("ケビン", 75, 100);
-    stu2.display();
   }
 }
