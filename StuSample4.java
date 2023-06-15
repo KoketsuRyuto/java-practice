24c8141800609f5716b78be825ac88acc5df4eda
Koketsu Ryuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-06-15 21:36:45 +0900

    finalクラス

--- StuSample4.java
+++ StuSample4.java
@@ -1,11 +1,9 @@
-class StuSample4 {
+public class StuSample4 {
   public static void main(String[] args) {
-    Student4.display();
-
-    Student4 stu1 = new Student4("ジョン");
-    Student4.display();
-
-    Student4 stu2 = new Student4("ケビン");
-    Student4.display();
+    final int BASE_NO = 1000;
+    Student4 stu1 = new Student4("ジョン", BASE_NO+1);
+    stu1.display();
+    Student4 stu2 = new Student4("ケビン", BASE_NO+2);
+    stu2.display();
   }
 }
