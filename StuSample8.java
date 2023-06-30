b6c2b8b4a78efeb93a4bba4fbc0cf28ca25684cd
Koketsu Ryuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-06-30 22:55:42 +0900

    パッケージ

---
+++ StuSample8.java
@@ -0,0 +1,6 @@
+public class StuSample8 {
+  public static void main(String[] args) {
+    Student8 stu = new Student8("ジョン");
+    stu.display();
+  }
+}
---
+++ pack/Student8.java
@@ -0,0 +1,12 @@
+
+public class Student8 {
+  private String name;
+
+  public Student8(String name) {
+    this.name = name;
+  }
+
+  public void display() {
+    System.out.println("名前；" + name);
+  }
+}
